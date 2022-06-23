bf122aa2b280cbfcc8b941f853ce3206159ad06e
Howard Hinestroza <froyrobledo99@gmail.com>
2022-06-22 21:30:29 -0500

    Se Agregan Rutas Principales

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from '../app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  exports: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, AppRoutingModule],
+})
+export class AuthModule {}
